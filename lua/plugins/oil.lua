fe0cbbde4b96894fac3063838a3b2c909e5900d9
rwiv <zc798546@gmail.com>
2026-02-15 18:40:30 +0900

    feat: add oil.nvim

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,8 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  lazy = false,
+  keys = {
+    { "-", "<CMD>Oil<CR>", desc = "Open Parent Directory" },
+  },
+}
