95fb643ea1abeadb1bb35bf27c1c384af43f2156
rwiv <zc798546@gmail.com>
2026-02-15 19:49:27 +0900

    feat: update neo-tree configs

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_ignored = true, -- hide files that are ignored by other gitignore-like files
+          hide_by_name = {
+            ".git",
+          },
+        },
+      },
+    },
+  },
+}
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,6 +1,24 @@
 return {
   "folke/snacks.nvim",
   opts = {
+    dashboard = {
+      preset = {
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = "", key = "p", desc = "Projects", action = ":lua Snacks.picker.projects()" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+      sections = {
+        { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1, limit = 8 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1, limit = 3 },
+        { section = "startup" },
+      },
+    },
     picker = {
       sources = {
         files = {
