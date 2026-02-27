6fb18fb5cd6f2705db24ca7a4e0ab0cef4e619e9
rwiv <zc798546@gmail.com>
2026-02-28 04:15:45 +0900

    feat: update colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -19,32 +19,9 @@ return {
       },
       color_overrides = {
         all = {
-          rosewater = "#acb2be", -- LazyGit cursor
-          flamingo = "#acb2be", -- '>' (snacks find file)
-          pink = "#be5046", -- !, ?
-          mauve = "#b477cf", -- keywords
-          red = "#bf956a", -- this(or self), error diagnostics
-          maroon = "#dce0e5", -- parameters
-          peach = "#dfc184", -- constants
-          yellow = "#6eb4bf", -- class, go package, rust self, lint diagnostics
-          green = "#a1c181", -- strings
-          teal = "#dfc184", -- python docstring, warning diagnostics, rust enum variant (e.g. Ok, Err...)
-          sky = "#6eb4bf", -- =, ==, |, &&, TODO comments, python initializer
-          sapphire = "#a1c181", -- ui folder color
-          blue = "#73ade9", -- functions
-          lavender = "#d07277", -- attibutes
-          text = "#acb2be",
-          subtext1 = "#dce0e5",
-          subtext0 = "#dce0e5",
-          overlay2 = "#878e98", -- comments, brackets
-          overlay1 = "#878e98",
-          overlay0 = "#5d636f", -- suggestion, hint text
-          surface2 = "#2f343e",
-          surface1 = "#5d636f", -- gitsigns
-          surface0 = "#2f343e", -- line background, hint background
-          base = "#282c33",
-          mantle = "#2f343e",
-          crust = "#3b414d",
+          base = "#1e1e1e",
+          mantle = "#222222",
+          crust = "#222222",
         },
       },
     },
