fb5f797ba04a76bef50dfb5fd83d669224555779
rwiv <zc798546@gmail.com>
2026-03-01 21:44:11 +0900

    feat: update markdown configs

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -31,9 +31,6 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     -- disable autoformat for markdown files, as it can cause unwanted formatting changes
     vim.b.autoformat = false
 
-    -- disable conceal for markdown files, as it can cause issues with displaying certain characters and formatting
-    vim.opt_local.conceallevel = 0
-
     -- enable wrap and linebreak for markdown files, as they are often used for writing long-form content
     vim.opt_local.wrap = true
     vim.opt_local.linebreak = true
--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,7 +1,22 @@
 return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
-    enabled = false,
+    opts = {
+      heading = { enabled = false },
+      paragraph = { enabled = false },
+      -- code = { enalbed = false },
+      dash = { enabled = false },
+
+      bullet = { enabled = false },
+      checkbox = { enabled = false },
+      quote = { enabled = false },
+      -- pipe_table = { enabled = false },
+      -- callout = { enabled = false },
+
+      -- link = { enabled = false },
+      sign = { enabled = false },
+      indent = { enabled = false },
+    },
   },
   {
     "iamcco/markdown-preview.nvim",
