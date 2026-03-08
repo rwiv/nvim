9a7a729fc4243bff1dfdced021f98431a41155f6
rwiv <zc798546@gmail.com>
2026-03-08 18:27:32 +0900

    feat: update auto-save config

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -3,5 +3,11 @@ return {
   event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
   opts = {
     enabled = true,
+    condition = function(buf)
+      if vim.fn.getbufvar(buf, "&buftype") ~= "" then
+        return false
+      end
+      return true
+    end,
   },
 }
